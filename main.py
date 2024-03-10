efad828f6400a3aea650ac0d12c58ed5075056ca
Hakim Horairy <horairyhakim@gmail.com>
2024-03-10 14:23:38 +0100

    starting main function

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import numpy
 import random
+import copy
+
 random.seed(1)
 
-grid = numpy.zeros(shape=(8,8))
+
 knight_position = random.randint(0, 7), random.randint(0,7)
 goal_position = random.randint(0, 7), random.randint(0, 7)
 
-grid[knight_position] = 1
-grid[goal_position] = 2
-
-print(grid)
 
 
+def get_shortest_path_length(knight_position, goal_position, current_path=[]):
 
+	shortest_path_length = float("inf")
+
+	possible_moves = get_possible_moves(knight_position)
+
+	for possible_move in possible_moves:
+		current_path.append(possible_move)
+
+		if current_path[-1] == goal_position and len(current_path) < shortest_path_length :
+			shortest_path = copy.deepcopy(current_path)
+			shortest_path_length = len(shortest_path)
+	
